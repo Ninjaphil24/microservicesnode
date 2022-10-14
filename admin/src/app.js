bf5f86170fcd1b8947c3017cd0fd11a8677fcb0b
unknown <philipmodino@gmail.com>
2022-10-14 21:52:49 +0200

    Install typeorm

--- admin/src/app.js
+++ admin/src/app.js
@@ -1,1 +1,12 @@
-console.log('hello');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var cors = require("cors");
+// import exp = require('constants')
+var app = express();
+app.use(cors({
+    origin: ['http://localhost:4200']
+}));
+app.use(express.json());
+console.log('Listening to port: 8000');
+app.listen(port, 8000);
--- admin/src/app.ts
+++ admin/src/app.ts
@@ -1,1 +1,14 @@
-console.log('hello')
+import * as express from 'express'
+import * as cors from 'cors'
+// import exp = require('constants')
+
+const app = express()
+
+app.use(cors({
+    origin: ['http://localhost:4200']
+}))
+
+app.use(express.json())
+
+console.log('Listening to port: 8000')
+app.listen(port:8000)
