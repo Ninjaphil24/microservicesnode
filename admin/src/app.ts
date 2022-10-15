1219c80997b863e1becfde5a2760d94139c9ed30
unknown <philipmodino@gmail.com>
2022-10-15 10:57:14 +0200

    Connect db and use postman

--- admin/src/app.ts
+++ admin/src/app.ts
@@ -1,8 +1,11 @@
 import * as express from 'express'
+import {Request,Response} from 'express'
 import * as cors from 'cors'
 import { createConnection } from 'typeorm'
+import {Product} from "./entity/product"
 
 createConnection().then(db=>{
+    const prodcutRepository=db.getRepository(Product);
     const app = express()
 
     app.use(cors({
@@ -10,6 +13,17 @@ createConnection().then(db=>{
     }))
 
     app.use(express.json())
+
+    app.get('/api/products', async (req: Request, res: Response)=>{
+        const products= await prodcutRepository.find()
+        res.json(products)
+    })
+
+    app.post('/api/products', async (req: Request, res: Response)=>{
+        const product= await prodcutRepository.create(req.body)
+        const result = await prodcutRepository.save(product)
+        return res.send(result)
+    }) 
 
     console.log('Listening to port: 8000')
     app.listen(8000)
